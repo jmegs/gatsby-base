3e30e63ecfc5873d18371c07d516f53ba0eeb4cf
John Meguerian <johnmeguerian@gmail.com>
2019-03-02 11:57:32 -0500

    prune plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,34 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-less`,
+      options: {
+        postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })]
+      }
+    },
+    {
+      // tell webpack where to allow you to import modules from an absolute
+      // project path rather than relative ../../ paths.
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        components: path.join(__dirname, 'src/components')
+      }
+    },
+    {
+      // enable mdx – a cool blend of markdown and JSX
+      resolve: 'gatsby-mdx',
+      options: {
+        extensions: ['.mdx', '.md']
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/img`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
-        displayName: true,
-        fileName: false
       }
     }
   ]

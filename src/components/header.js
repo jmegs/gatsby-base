fb88818cb7b74e9855e9b8db85b18a97942bf2de
John Meguerian <johnmeguerian@gmail.com>
2019-03-02 12:40:25 -0500

    wire up examples and add base css

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Gatsby Base</h1>
+      <nav>
+        <a href="https://github.com/jmegs/gatsby-base">Github</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from 'components/header'
 
 const IndexPage = () => {
   return (
     <React.Fragment>
+      <Header />
       <h1>hello world!</h1>
     </React.Fragment>
   )
